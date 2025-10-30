eb87b87329f39fd2947c3365ca771e2363819195
nhnn3106 <120386489+nhnn3106@users.noreply.github.com>
2025-10-30 13:08:34 +0700

    Clean up PhoneService.java comments and package
    Removed unnecessary comments and updated package declaration.

--- Bai02/src/main/java/iuh/fit/edu/services/PhoneService.java
+++ Bai02/src/main/java/iuh/fit/edu/services/PhoneService.java
@@ -2,12 +2,7 @@
  * @ (#) .java    1.0
  * Copyright (c)  IUH. All rights reserved.
  */
-package iuh.fit.edu.services;/*
- * @description
- * @author: Huu Thai
- * @date:
- * @version: 1.0
- */
+package iuh.fit.edu.services;
 
 import iuh.fit.edu.entities.Phone;
 import iuh.fit.edu.entities.Supplier;
