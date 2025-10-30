f09ec38fe42264bc21d0e18f28657305d2b366f4
nhnn3106 <120386489+nhnn3106@users.noreply.github.com>
2025-10-30 13:07:12 +0700

    Update PhoneRepository.java

--- Bai02/src/main/java/iuh/fit/edu/repositories/PhoneRepository.java
+++ Bai02/src/main/java/iuh/fit/edu/repositories/PhoneRepository.java
@@ -10,12 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-/*
- * @description
- * @author: Huu Thai
- * @date:
- * @version: 1.0
- */
+
 public interface PhoneRepository extends JpaRepository<Phone, Long> {
     List<Phone> findByNhaCungCap(Supplier nhaCungCap);
 }
